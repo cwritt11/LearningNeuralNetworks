7b18357bc17345aeee44956689674c3287a19ec4
Christopher Writt <cwritt11@gmail.com>
2022-07-15 13:16:22 -0400

    softmax

--- aLayer.py
+++ aLayer.py
@@ -4,12 +4,6 @@ from nnfs.datasets import spiral_data
 
 nnfs.init()
 
-X = [[1.0, 2.0, 3.0, 2.5],
-	 [2.0, 5.0, -1.0, 2.0],
-     [-1.5, 2.7, 3.3, -0.8]]
-
-X, y = spiral_data(100, 3)
-
 class Layer_Dense:
 	def __init__(self, n_inputs, n_neurons):
 		self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
@@ -17,57 +11,28 @@ class Layer_Dense:
 	def forward(self, inputs):
 		self.output = np.dot(inputs, self.weights) + self.biases
 
-
 class Activation_ReLU:
 	def forward(self, inputs):
 		self.output = np.maximum(0, inputs)
 
+class Activation_Softmax:
+	def forward(self, inputs):
+		exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+		probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+		self.output = probabilities
 
-layer1 = Layer_Dense(2, 5)
+X, y = spiral_data(samples=100, classes=3)
+
+dense1 = Layer_Dense(2,3)
 activation1 = Activation_ReLU()
 
-layer1.forward(X)
-# print(layer1.output)
-activation1.forward(layer1.output)
-print(activation1.output)
+dense2 = Layer_Dense(3,3)
+activation2 = Activation_Softmax()
 
+dense1.forward(X)
+activation1.forward(dense1.output)
 
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-def create_data(points, classes):
-	X = np.zeros((points*classes, 2))
-	y = np.zeros(points*classes, dtype='uint8')
-	for class_number in range(classes):
-		ix = range(points*class_number, points*(class_number+1))
-		r = np.linspace(0.0, 1, points)
-		t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
-		X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
-		y[ix] = class_number
-	return X,y
-'''
+print(activation2.output[:5])
